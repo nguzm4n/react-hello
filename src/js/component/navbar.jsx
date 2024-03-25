d431bfd614f62c94cf1cbc4f2ac7aa3b49ff4033
Nicolás Guzmán <nguzmanespinoza1@gmail.com>
2024-03-25 18:11:11 -0300

    Navbar Lista

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function NavBar() {
     return (
-        <nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <div class="container-fluid ">
     <a class="navbar-brand text-light" href="#">Start BootStrap</a>
-    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon text.body-secondary "></span>
     </button>
-    <div class="collapse navbar-collapse " id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse  " id="navbarNav">
+      <ul class="navbar-nav  ms-auto">
         <li class="nav-item">
           <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
         </li>
@@ -22,8 +22,8 @@ function NavBar() {
         <li class="nav-item">
           <a class="nav-link text-light" href="#">Contact</a>
         </li>
-        
       </ul>
+      
     </div>
   </div>
 </nav>
